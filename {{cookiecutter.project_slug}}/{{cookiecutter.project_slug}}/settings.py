528b611a3ff30d04137cb4f89fbb68a06844da58
Robson Santos <robson.santos@smartgreen.net>
2019-03-25 17:12:01 -0300

    ADD config dict

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
@@ -2,6 +2,7 @@ import os
 
 class Settings(object):
     TARTIFLETTE_SDL_PATH = os.path.dirname(os.path.abspath(__file__)) + "/sdl"
+
     TARTIFLETTE_MODULES = [
         '{{cookiecutter.project_slug}}.resolvers.query',
         '{{cookiecutter.project_slug}}.resolvers.mutation',
@@ -9,3 +10,14 @@ class Settings(object):
         '{{cookiecutter.project_slug}}.directives.rate_limiting',
         '{{cookiecutter.project_slug}}.directives.non_introspectable',
     ]
+
+    TARTIFLETTE_CONFIG = {
+        'subscription_ws_endpoint': '/ws',
+        'executor_http_endpoint': '/graphql',
+        'executor_http_methods': ['POST'],
+        'graphiql_enabled': True,
+        'graphiql_options': {
+            'endpoint': '/graphiql',
+            'default_query': '# Tartiflette Graphql Server\n\n',
+        }
+    }
