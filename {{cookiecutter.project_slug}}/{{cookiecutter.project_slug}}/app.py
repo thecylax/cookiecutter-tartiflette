cd60bfe55c4a3b8c8b0049ac5d1275248ffa32a8
Robson Santos <robson.santos@smartgreen.net>
2019-06-25 13:50:33 -0300

    REMOVE comentaries

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app.py
@@ -6,10 +6,6 @@ from tartiflette_aiohttp import register_graphql_handlers
 from {{cookiecutter.project_slug}}.settings import Settings
 
 settings = Settings()
-# engine = Engine(
-#     sdl=settings.TARTIFLETTE_SDL_PATH,
-#     modules=settings.TARTIFLETTE_MODULES,
-# )
 app = web.Application()
 registered_handlers = register_graphql_handlers(
     app=app,
